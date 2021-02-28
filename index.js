7c6e08f0a0698ba41c6f6b65a8644edd289b5915
NINJAPEACE <71932430+NINJAPEACE@users.noreply.github.com>
2021-02-28 18:03:00 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.on('ready', () => {
+ console.log(`Bot siap digunakan`);
+});
+
+client.on('message', async message => {
+  if(message.content === "Halo") {
+    message.reply(`Halo juga`);
+  }
+  if(message.content === "Hi") {
+    message.channel.send(`Hi juga`)
+  }
+});
+client.login(`Your Bot Token`)
