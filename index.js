607450dcdc388b9fcd65dd78d4bce6ad376dfb30
NINJAPEACE <71932430+NINJAPEACE@users.noreply.github.com>
2021-03-07 16:29:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const client = new discord.Client();
 client.on('ready', () => {
  console.log(`Bot siap digunakan`);
 });
-
+ tes hali halo
+ 
 client.on('message', async message => {
   if(message.content === "Halo") {
     message.reply(`Halo juga`);
