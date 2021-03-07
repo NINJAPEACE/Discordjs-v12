53af1ceb523cbde159c056e9bcaa976d4c1144a7
NINJAPEACE <71932430+NINJAPEACE@users.noreply.github.com>
2021-03-07 16:49:22 +0700

    Delete commands directory

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = async(client, message, args) => {
-    message.channel.send('Pong! _Ping saya ${client.ws.ping} ms_');
-};
-
-module.exports.config = {
-   name: 'ping',
-	 aliases: []
-};
