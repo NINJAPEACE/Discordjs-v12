3e1c2905400d29da2fa4bfcf61a9830220362010
NINJAPEACE <71932430+NINJAPEACE@users.noreply.github.com>
2021-03-07 16:39:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const discord = require('discord.js');
 const client = new discord.Client();
+const fs = require('fs');
+client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
+
+fs.readdir('./commands', (err, files) {
+    if(err) return console.log(err);
+
+    let js = files.filter(f => f.endsWith('.js'));
+    if(js.length <= 0) {
+       console.log('Tidak dapat menemukan command!');
+    };
+     
+    js.forEach((f, i) => {
+        let pull = require('./commands/${f}');
+          client.commands.set(pull.config.name, pull);
+          pull.config.aliases.forEach(alias => {
+           client.aliases.set(alias, pull.config.name);
+         });
+    });
+});
 
 client.on('ready', () => {
  console.log(`Bot siap digunakan`);
 });
- tes hali halo
  
 client.on('message', async message => {
   if(message.content === "Halo") {
